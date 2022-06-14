9b3732d5db0fcf65d107e72db279247da15aaabf
Nathan Fradet <56734983+Natooz@users.noreply.github.com>
2022-06-14 10:34:35 +0200

    setup update

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Nathan Fradet',
     url='https://github.com/Natooz/flexformer',
     packages=find_packages(exclude=("test",)),
-    version='1.0.0',
+    version='1.0.1',
     license='MIT',
     description='A general implementation of Transformer, to play around with attention and build custom architectures',
     long_description=long_description,
